8ec2f253f3e86c58ba8e3d02634d3eaaa63b11ea
Munishrn <46596615+Munishrn@users.noreply.github.com>
2026-05-08 20:04:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -694,4 +694,5 @@ def get_current_status(order_id):
 
 
 if __name__ == "__main__":
-    app.run(port=10000, debug=False)
+    import os
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)), debug=False)
